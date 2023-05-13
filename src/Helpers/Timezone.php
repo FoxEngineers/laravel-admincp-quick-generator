7dac72e1fb5d3c4fd5e4377255e80590e2cec66a
lamtranb <ltb58@open.ac.uk>
2023-05-14 00:19:31 +0700

    init

--- src/Helpers/Timezone.php
+++ src/Helpers/Timezone.php
@@ -9,26 +9,33 @@ use Carbon\Carbon;
  */
 class Timezone
 {
+    private string $userTimezone;
+
+    public function __construct(string $userTimezone)
+    {
+        $this->userTimezone = $userTimezone;
+    }
+
     /**
-     * @param string $userTimezone
      * @param Carbon $date
      * @param string $format
      *
      * @return Carbon
      */
-    public function convertToLocal(string $userTimezone, Carbon $date, string $format = 'Y-m-d H:i:s'): string
+    public function convertToLocal(Carbon $date, string $format = 'Y-m-d H:i:s'): string
     {
-        return $date->setTimezone($userTimezone ?? config('app.timezone'))->format($format);
+        return $date->setTimezone($this->userTimezone ?? config('app.timezone'))
+            ->format($format);
     }
 
     /**
-     * @param string $userTimezone
      * @param string $date
      *
      * @return Carbon
      */
-    public function convertFromLocal(string $userTimezone, string $date): Carbon
+    public function convertFromLocal(string $date): Carbon
     {
-        return Carbon::parse($date, $userTimezone)->setTimezone('UTC');
+        return Carbon::parse($date, $this->userTimezone)
+            ->setTimezone('UTC');
     }
 }
