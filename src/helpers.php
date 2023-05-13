8f69ef21027147872701863ad1a02f457bb1bcd8
lamtranb <ltb58@open.ac.uk>
2023-05-14 00:09:09 +0700

    init

--- src/helpers.php
+++ src/helpers.php
@@ -10,3 +10,11 @@ if (!function_exists('real_path')) {
         return str_starts_with($url, '/') ? url($url) : $url;
     }
 }
+
+if (!function_exists('help_text')) {
+    function help_text(string $text, string $url): string
+    {
+        $url = url($url);
+        return "<p>- {$text}: <a href='{$url}' target='_blank'>{$url}</a></p>";
+    }
+}
